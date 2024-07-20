24b35cbaec8b7ec6bf218653dbdd06f26bbb12f3
lorenzo <lorenzo@gmail.com>
2024-07-20 12:12:30 -0300

    img insert

--- cadastrar.php
+++ cadastrar.php
@@ -7,17 +7,64 @@ $email = $_POST['email'];
 $senha = $_POST['senha'];
 $nome = $_POST['nome'];
 
+$pastaDestino = "/uploads/";
 
-$sql = "INSERT INTO usuario (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
-$resultado = mysqli_query($conexao, $sql);
-if ($resultado === false) {
-    if (mysqli_errno($conexao) == 1062) {
-        echo "Email já cadastrado no sistema! Tente fazer o login ou faça a recuperação de senha.";
-        die();
-    } else {
-        echo "Erro ao inserir o novo usuário!" . mysqli_errno($conexao) . ": " . mysqli_error($conexao);
-        die();
-    }
+// verificar se o tamanho do arquivo é maior que 2 MB
+if ($_FILES['arquivo']['size'] > 2000000) {  // condição de guarda 👮
+    echo "O tamanho do arquivo é maior que o limite permitido. Limite máximo: 2 MB.";
+    die();
 }
 
-header("Location: index.php");
+// verificar se o arquivo é uma imagem
+$extensao = strtolower(pathinfo($_FILES['arquivo']['name'], PATHINFO_EXTENSION));
+
+if (
+    $extensao != "png" && $extensao != "jpg" &&
+    $extensao != "jpeg" && $extensao != "gif" &&
+    $extensao != "jfif" && $extensao != "svg"
+) { // condição de guarda 👮
+    echo "O arquivo não é uma imagem! Apenas selecione arquivos 
+    com extensão png, jpg, jpeg, gif, jfif ou svg.";
+    die();
+}
+
+// verificar se é uma imagem de fato
+if (getimagesize($_FILES['arquivo']['tmp_name']) === false) {
+    echo "Problemas ao enviar a imagem. Tente novamente.";
+    die();
+}
+
+$nomeArquivo = uniqid();
+
+// se deu tudo certo até aqui, faz o upload
+$fezUpload = move_uploaded_file(
+    $_FILES['arquivo']['tmp_name'],
+    __DIR__ . $pastaDestino . $nomeArquivo . "." . $extensao
+);
+if ($fezUpload == true) {
+    $sql = "INSERT INTO usuarios(nome, email, senha, foto) VALUES ('$nome', '$email', '$senha', '$nomeArquivo.$extensao')";
+    $resultado = mysqli_query($conexao, $sql);
+    if ($resultado != false) {
+        // se for uma alteração de arquivo
+        if (isset($_POST['arquivo'])) {
+            $apagou = unlink(__DIR__ . $pastaDestino . $_POST['arquivo']);
+            if ($apagou == true) {
+                $sql = "DELETE FROM usuarios WHERE foto='" 
+                        . $_POST['arquivo'] . "'";
+                $resultado2 = mysqli_query($conexao, $sql);
+                if ($resultado2 == false) {
+                    echo "Erro ao apagar o arquivo do banco de dados.";
+                    die();
+                }
+            } else {
+                echo "Erro ao apagar o arquivo antigo.";
+                die();
+            }
+        }
+        header("Location: index.php");
+    } else {
+        echo "Erro ao registrar o arquivo no banco de dados.";
+    }
+} else {
+    echo "Erro ao mover arquivo.";
+}
